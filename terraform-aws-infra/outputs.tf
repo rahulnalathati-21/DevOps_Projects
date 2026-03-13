fa5be5eaa1dfc9a14c1cd6720e46cd300407f0a7
Rahul <rahulsiddharth2.0@gmail.com>
2026-03-13 16:49:10 +0530

    remove s3 output since module removed

--- terraform-aws-infra/outputs.tf
+++ terraform-aws-infra/outputs.tf
@@ -10,6 +10,6 @@ output "instance_public_ip" {
   value = module.ec2.instance_public_ip
 }
 
-output "s3_bucket" {
-  value = module.s3.bucket_name
-}
+# output "s3_bucket" {
+  # value = module.s3.bucket_name
+# }
